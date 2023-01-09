b94d116e3f4fbdaacc5662258ac06f492aef0f3a
Lucas Soares Maciel <lucas.tibetano@gmail.com>
2023-01-09 19:11:01 -0300

    Criação do model user e das rotas de crud(sem implementação)

---
+++ src/app/controllers/authController.js
@@ -0,0 +1,27 @@
+const express = require('express')
+
+const router = express.Router()
+
+
+router.post('/', (req, res) => {
+    res.status(200).json({})
+})
+
+router.put('/', (req, res) => {
+    res.status(200).json({})
+})
+
+router.delete('/', (req, res) => {
+    res.status(200).json({})
+})
+
+router.get('/', (req, res) => {
+    res.status(200).json({})
+})
+
+router.get('/', (req, res) => {
+    res.status(200).json({})
+})
+
+
+
---
+++ src/app/models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        lowercase: true,
+        required: true
+    },
+    cpf: {
+        type: String,
+        unique: true,
+        required: true,
+        select: false
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+User = mongoose.model('User', UserSchema)
+
+module.exports = User
